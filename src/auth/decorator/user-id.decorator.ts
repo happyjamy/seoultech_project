6bd97b6bcfa1b4fc85017f1c38773344cb4540c2
juwhan <juwhan1004@naver.com>
2022-11-29 14:37:53 +0000

    add exception

--- src/auth/decorator/user-id.decorator.ts
+++ src/auth/decorator/user-id.decorator.ts
@@ -1,8 +1,11 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext, UnauthorizedException } from '@nestjs/common';
 
 export const UserId = createParamDecorator(
   (data: unknown, context: ExecutionContext) => {
-    const userId = context.switchToHttp().getRequest().user._id.toString();
-    return userId;
+    const userId = context.switchToHttp().getRequest().user._id;
+    if(!userId) {
+      throw new UnauthorizedException("user does not exist");
+    }
+    return userId.toString();
   },
 );
