3c38e504b1f454591cea833516ae2bbdc2346e79
happyjamy <juwhan1004@naver.com>
2022-11-30 01:53:12 +0900

    add swagger decorator

--- src/common/dto/common.dto.ts
+++ src/common/dto/common.dto.ts
@@ -1,40 +1,56 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 import { Types } from 'mongoose';
 
 export class CreateCommonDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   content: string;
 
+  @ApiProperty()
   @IsString({ each: true })
   @IsNotEmpty()
   imageUrl: string[];
 }
 
 export class UpdateCommonDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   content: string;
 
+  @ApiProperty()
   @IsString({ each: true })
   @IsNotEmpty()
   imageUrl: string[];
 }
 
 export class CreateCommon {
+  @ApiProperty()
   _id: Types.ObjectId;
+  @ApiProperty()
   title: string;
+  @ApiProperty()
   content: string;
+  @ApiProperty()
   imageUrl: string[];
+  @ApiProperty()
   authorId: string;
+  @ApiProperty()
   createdAt: Date;
+  @ApiProperty()
   updatedAt: Date;
+  @ApiProperty()
+  viewCount: number;
 }
